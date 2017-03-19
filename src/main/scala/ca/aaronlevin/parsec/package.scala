f1d0a69f99acb18abaec9bc5df9beec3021cb4ed
aaron levin <aaron.levin@soundcloud.com>
2017-03-19 23:46:12 +0100

    fix sbt-fresh's weird package convention

--- src/main/scala/ca/aaronlevin/parsec/package.scala
+++ src/main/scala/ca/aaronlevin/parsec/package.scala
@@ -19,7 +19,7 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-package ca.aaronlevin.scala
+package ca.aaronlevin
 
 import cats.{ Functor, Monad }
 import scala.language.higherKinds
